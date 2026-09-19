763cdd1e143dc056c34b8d4a87beec0d08334c03
Kaitlyn Nam 2/6/25 <kaitlynnam62@gmail.com>
2026-09-19 09:04:52 -0400

    changed teleop to extend opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Teleop.java
@@ -4,7 +4,7 @@ import com.pedropathing.drivetrain.DrivePowers;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.ManualDrive;
 import com.pedropathing.math.Pose;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.config.ConfigInfo;
@@ -12,38 +12,50 @@ import org.firstinspires.ftc.teamcode.input.InputHandler;
 import org.firstinspires.ftc.teamcode.pedro.Constants;
 
 @TeleOp(name = "Teleop", group = "Competition")
-public class Teleop extends LinearOpMode {
+public class Teleop extends OpMode {
     private Follower follower;
     private InputHandler input;
 
     @Override
-    public void runOpMode() {
+    public void init() {
         follower = Constants.create(hardwareMap);
         follower.setPose(ConfigInfo.Field.ORIGIN);
         input = new InputHandler(gamepad1, gamepad2);
 
         telemetry.addLine("Initialized. Press start.");
         telemetry.update();
+    }
 
-        waitForStart();
-        if (isStopRequested()) return;
-
+    @Override
+    public void start() {
+        // Refresh localization before the first field-centric command.
+        follower.update();
         input.reset();
+    }
 
-        while (opModeIsActive()) {
-            input.update();
+    @Override
+    public void loop() {
+        input.update();
 
-            if (input.headingResetRequested()) follower.setHeading(0);
+        if (input.headingResetRequested()) follower.setHeading(0);
 
-            DrivePowers powers = input.drivePowers();
-            if (input.fieldCentric()) {
-                powers = ManualDrive.fieldCentric(powers, follower.pose().heading());
-            }
+        DrivePowers powers = input.drivePowers();
+        if (input.fieldCentric()) {
+            powers = ManualDrive.fieldCentric(powers, follower.pose().heading());
+        }
 
-            follower.manual(powers);
+        follower.manual(powers);
+        follower.update();
+
+        addTelemetry();
+    }
+
+    @Override
+    public void stop() {
+        if (follower != null) {
+            follower.stop();
+            // Pedro 3 applies the idle drivetrain stop during update().
             follower.update();
-
-            addTelemetry();
         }
     }
 
